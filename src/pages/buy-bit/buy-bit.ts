8f3cd9a2c672b446585c931a6a757f35c36e7d6d
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2018-12-04 15:14:30 +0100

    Open whatsapp to chat with the seller.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/pages/buy-bit/buy-bit.ts
+++ src/pages/buy-bit/buy-bit.ts
@@ -173,8 +173,8 @@ export class BuyBitPage {
     }
 
     sendExternalMessage(seller) {
-        //let phoneNumber = seller.seller.phoneNumber;
-        let phoneNumber = '2348025943549';
+        let phoneNumber = seller.seller.phoneNumber;
+        //let phoneNumber = '2348025943549';
         if (phoneNumber === undefined || phoneNumber === null) {
             Constants.showLongerToastMessage("You can't chat with this seller.", this.toastCtrl);
         } else {
@@ -183,7 +183,7 @@ export class BuyBitPage {
             let priceFrom = seller.amountToSell;
             let priceTo = seller.amountToRecieve;
             let rate = seller.rate;
-            let message = "I'm is interested in your " + priceFrom + coin +  " -> " + priceTo + toCoin + " (@ " + rate + ") trade posted on XendBit";
+            let message = "I'm interested in your " + priceFrom + coin +  " -> " + priceTo + toCoin + " (@ " + rate + ") trade posted on XendBit";
             let url = "https://wa.me/" + phoneNumber + "?text=" + message;
             const browser = this.iab.create(url, "_system", "hardwareback=yes,");
             browser.close();
