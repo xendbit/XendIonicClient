ef40b34edf9845ac5ac2b55592b1827834b57314
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-01-20 00:32:13 +0100

    Remove footer in login page
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/pages/utils/constants.ts
+++ src/pages/utils/constants.ts
@@ -4,10 +4,10 @@ import { LocalProps } from "./localprops";
 import { Wallet, Fees } from "./wallet";
 
 export class Constants {    
-    //static TOMCAT_URL = "http://34.70.143.198:8081";
+    static TOMCAT_URL = "http://34.70.143.198:8081";
     static APP_VERSION = "v4.6-rc31"
     static ENABLE_GUEST = false;
-    static TOMCAT_URL = "http://localhost:9091";
+    //static TOMCAT_URL = "http://localhost:9091";
     //static TOMCAT_URL = "https://lb.xendbit.com";
     //static TOMCAT_URL = "https://xendfilb.xendbit.net";
     static XEND_BASE_URL = Constants.TOMCAT_URL + "/api/";
